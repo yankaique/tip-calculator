6e0f45c222d6e1a57f617cbc254107535fe66a42
Yan <yankaiquecosta.yk@gmail.com>
2020-02-02 08:32:45 -0300

    Tip percentage added

--- App.js
+++ App.js
@@ -43,15 +43,26 @@ const ResultItem = styled.Text`
   margin-bottom:10px;
 `;
 
+const PctArea = styled.View`
+  flex-direction:row;
+  margin:20px;
+  padding:15px;
+`;
+
+const PctItem = styled.Button`
+    
+`;
+
 const App = () => {
 
   const [bill, setBill] = useState('');
   const [tip, setTip] = useState(0);
+  const [pct, setPct] = useState(10);
 
   const calc = () => {
     let nBill = parseFloat(bill);
     if (nBill) {
-      setTip(nBill * 0.1);
+      setTip((pct/100)*nBill);
     } else {
       alert("Enter account amount");
     }
@@ -66,13 +77,19 @@ const App = () => {
         value={bill}
         onChangeText={n => setBill(n)}
       />
-      <CalcButton title="Calculate" onPress={calc} />
+      <PctArea>
+        <PctItem title="5%" onPress={()=>setPct(5)}/>
+        <PctItem title="10%" onPress={()=>setPct(10)}/>
+        <PctItem title="15%" onPress={()=>setPct(15)}/>
+        <PctItem title="20%" onPress={()=>setPct(20)}/>
+      </PctArea>
+      <CalcButton title={`Calculate (${pct}%)`} onPress={calc} />
       {tip > 0 &&
         <ResultArea>
           <ResultItemTitle>Bill Amount</ResultItemTitle>
           <ResultItem>${parseFloat(bill).toFixed(2)}</ResultItem>
           <ResultItemTitle>Tip Amount</ResultItemTitle>
-          <ResultItem>${tip.toFixed(2)} (10%)</ResultItem>
+          <ResultItem>${tip.toFixed(2)} ({pct}%)</ResultItem>
           <ResultItemTitle>Final Value</ResultItemTitle>
           <ResultItem>${(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
         </ResultArea>
