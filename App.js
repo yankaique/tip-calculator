5ac83c0539151b293304206057d1d352407df7bf
Yan <yankaiquecosta.yk@gmail.com>
2020-02-02 08:18:35 -0300

    Function calc(calculate), button and bill area added

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { } from 'react-native';
 import styled from 'styled-components/native'
 
@@ -19,22 +19,65 @@ const Input = styled.TextInput`
   font-size:18px;
   background-color:#EEE;
   padding:10px;
+`;
 
+const CalcButton = styled.Button`
+  margin-top:10px;
+`;
+
+const ResultArea = styled.View`
+  width:90%;
+  margin-top:30px;
+  background-color:#EEE;
+  justify-content:center;
+  align-items:center;
+`;
+
+const ResultItemTitle = styled.Text`
+  font-size:18px;
+  font-weight:bold;
+`;
+
+const ResultItem = styled.Text`
+  font-size: 15px;
+  margin-bottom:10px;
 `;
 
 const App = () => {
 
   const [bill, setBill] = useState('');
+  const [tip, setTip] = useState(0);
+
+  const calc = () => {
+    let nBill = parseFloat(bill);
+    if (nBill) {
+      setTip(nBill * 0.1);
+    } else {
+      alert("Enter account amount");
+    }
+  }
 
   return (
     <Page>
       <HeaderText>Tip Calculator</HeaderText>
-      <Input 
+      <Input
         placeholder="Bill amount"
         keyboardType="numeric"
         value={bill}
-        onChangeText={n=>setBill(n)}
+        onChangeText={n => setBill(n)}
       />
+      <CalcButton title="Calculate" onPress={calc} />
+      {tip > 0 &&
+        <ResultArea>
+          <ResultItemTitle>Bill Amount</ResultItemTitle>
+          <ResultItem>${parseFloat(bill).toFixed(2)}</ResultItem>
+          <ResultItemTitle>Tip Amount</ResultItemTitle>
+          <ResultItem>${tip.toFixed(2)} (10%)</ResultItem>
+          <ResultItemTitle>Final Value</ResultItemTitle>
+          <ResultItem>${(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+        </ResultArea>
+      }
+
     </Page>
   );
 };
